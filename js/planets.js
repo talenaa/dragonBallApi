a420da616728b0f51854ee4c9e9000b128c03948
talenaa <talenabarbosa7@gmail.com>
2024-11-05 12:01:40 +0100

    style: changed some js texts and added header main footer to the html

---
+++ js/planets.js
@@ -0,0 +1,45 @@
+const requestURL = 'https://dragonball-api.com/api/planets?limit=20';
+
+async function fetchPlanetsJson(){
+    const response = await fetch(requestURL);
+    try{
+        if (!response.ok) {
+            throw new Error(`Error in the Json request ${response.status}`);
+        }
+        return await response.json();
+    }
+    catch (error){
+        console.error('Error getting planets from the API : ', error);
+        return null;
+    }
+   
+}
+
+function createPlanetsCard ({ name, isDestroyed, image}){
+    return `
+        <div class="card" style="width: 200px;">
+            <img src="${image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <p class="card-text">${isDestroyed}</p>
+            </div>
+        </div>
+`;
+}
+
+async function displayPlanets() {
+    const planetsSection = document.getElementById('planetSection');
+    const planetsData = await fetchPlanetsJson();
+
+    if (planetsData && planetsData.items){
+        const planetsCards = planetsData.items.map(createPlanetsCard).join('');
+        planetsSection.innerHTML = planetsCards;
+    }
+    else
+    {
+        planetsSection.innerHTML = `<p>The Json of the planets could not be loaded</p>`;    
+    }
+}
+
+
+displayPlanets();
